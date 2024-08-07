56e055e9c1ff95738bd1568e446234eb286e5e62
Jiwoong <dnd0311@naver.com>
2024-08-07 16:05:02 +0900

    finalized

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,37 @@
-export class User {}
+import { BaseEntity } from '../../common/base.entity';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import * as gravatar from 'gravatar';
+import { InternalServerErrorException } from '@nestjs/common';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+  @Column()
+  public profileImg?: string;
+
+  @BeforeInsert()
+  async beforeSaveFunction() {
+    try {
+      const saltValue = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, saltValue);
+
+      this.profileImg = gravatar.url(this.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+        protocol: 'https',
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  @InjectRepository(User)
+  private readonly userRepository: Repository<User>;
+
+  async getUserById(id: string) {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async getUserByEmail(email: string) {
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async createUser(createUserDto: CreateUserDto) {
+    const newUser = await this.userRepository.create(createUserDto);
+    await this.userRepository.save(newUser);
+    return newUser;
   }
 }
