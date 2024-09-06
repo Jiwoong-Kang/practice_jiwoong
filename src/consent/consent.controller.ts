1f615c3ea875381fd3c7561418c3af63359c9425
Jiwoong <dnd0311@naver.com>
2024-09-05 22:20:09 -0600

    Add new consents

--- src/consent/consent.controller.ts
+++ src/consent/consent.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { ConsentService } from './consent.service';
+import { JwtAuthGuard } from '@auth/guards/jwt-auth.guard';
+import { RequestWithUser } from '@auth/interfaces/requestWithUser.interface';
+import { CreateConsentDto } from '@consent/dto/create-consent.dto';
 
 @Controller('consent')
 export class ConsentController {
   constructor(private readonly consentService: ConsentService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async createConsent(
+    @Req() req: RequestWithUser,
+    @Body() createConsentDto: CreateConsentDto,
+  ) {
+    return await this.consentService.createConsent(req.user, createConsentDto);
+  }
+
+  @Put()
+  @UseGuards(JwtAuthGuard)
+  async updateConsentByUser(
+    @Req() req: RequestWithUser,
+    @Body() createConsentDto: CreateConsentDto,
+  ) {
+    return await this.consentService.updateConsentByUser(
+      req.user,
+      createConsentDto,
+    );
+  }
 }
--- src/consent/consent.service.ts
+++ src/consent/consent.service.ts
@@ -2,11 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Consent } from '@consent/entities/consent.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserService } from '@user/user.service';
+import { User } from '@user/entities/user.entity';
+import { CreateConsentDto } from '@consent/dto/create-consent.dto';
 
 @Injectable()
 export class ConsentService {
   constructor(
     @InjectRepository(Consent)
     private readonly consentRepository: Repository<Consent>,
+    private readonly userService: UserService,
   ) {}
+
+  async createConsent(user: User, createConsentDto: CreateConsentDto) {
+    const newConsent = await this.consentRepository.create({
+      ...createConsentDto,
+      user,
+    });
+    await this.consentRepository.save(newConsent);
+    return newConsent;
+  }
+
+  async updateConsentByUser(user: User, createConsentDto: CreateConsentDto) {
+    const userInfo = await this.userService.getUserById(user.id);
+    return await this.consentRepository.update(
+      { id: userInfo.consent.id },
+      createConsentDto,
+    );
+  }
 }
